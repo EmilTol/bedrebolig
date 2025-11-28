ce4007e128ce8dc9816e23d9a652931993dee653
Emil <erazertt@gmail.com>
2025-11-28 10:02:00 +0100

    Img framework

---
+++ middleware/imagesUpload.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, path.join(__dirname, "..", "public", "images"));
+    },
+    filename: (req, file, callback) => {
+        const uniqueId = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const ext = path.extname(file.originalname);
+        callback(null, path.join(__dirname, "..", "public", "images", uniqueId, ext));
+    }
+});
+
+const fileFilter = (req, file, callback) => {
+    if (file.mimetype.startsWith("image/")) {
+        callback(null, true);
+    } else {
+        callback(new Error("Invalid file type"));
+    }
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
